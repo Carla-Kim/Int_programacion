2cbc69c2fed4114143130b0d9b6e32ce357a8da6
Carla Kim <carlakim138@gmail.com>
2023-06-11 18:52:47 -0300

    add: new fuctions - Queue

--- guia_10.py
+++ guia_10.py
@@ -1,3 +1,6 @@
+import random
+from queue import LifoQueue as Pila
+
 '''
 notas:
 - read() -> ("perro /ngato /nsapo")
@@ -8,7 +11,7 @@ notas:
 
 '''
 
-#1.2
+#1.1.2
 def contarLineas(archivoEnviado: str) -> int:
     archivo = open(archivoEnviado, "r")
     contador: int = 0
@@ -18,9 +21,9 @@ def contarLineas(archivoEnviado: str) -> int:
     archivo.close()
     return contador
     
-print(contarLineas("archivotest"))
+#print(contarLineas("archivotest.txt"))
 
-#1.2: notar que la linea es "texto texto" y no "texto", "texto"
+#1.1.2: notar que la linea es "texto texto" y no "texto", "texto"
 def existePalabra(palabraEnviada: str, archivoEnviado: str) -> bool:
     archivo = open(archivoEnviado, "r")
     listaDeLineas: list[str] = archivo.readlines()
@@ -30,9 +33,9 @@ def existePalabra(palabraEnviada: str, archivoEnviado: str) -> bool:
     archivo.close()
     return False
 
-print(existePalabra("texto", "archivotest"))
+#print(existePalabra("texto", "archivotest.txt"))
 
-#1.3
+#1.1.3
 def cantidadApariciones(archivoEnviado: str, palabraEnviada: str) -> int:
     cantidadApariciones: int = 0
     archivo = open(archivoEnviado, "r")
@@ -40,7 +43,7 @@ def cantidadApariciones(archivoEnviado: str, palabraEnviada: str) -> int:
     listaDePalabras: list[str] = []
     
     for lista in contenido:
-        listaDePalabras += lista.split()
+        listaDePalabras += lista.split() #+= une las palabras/elementos por separado
         
     for palabra in listaDePalabras:
         if palabraEnviada in palabra:
@@ -49,9 +52,9 @@ def cantidadApariciones(archivoEnviado: str, palabraEnviada: str) -> int:
     
     return cantidadApariciones
 
-print(cantidadApariciones("archivotest", "texto"))
+#print(cantidadApariciones("archivotest.txt", "texto"))
 
-2.2
+#1.2
 def clonarSinComentarios(nombre_archivo: str):
     archivo = open(nombre_archivo, "r")
     contenido = archivo.readlines()
@@ -69,4 +72,75 @@ def clonarSinComentarios(nombre_archivo: str):
     archivo.close()
     destino.close()
 
-print(clonarSinComentarios("archivotest.txt"))
+#print(clonarSinComentarios("archivotest.txt"))
+
+#1.3
+def textoReverso(nombre_archivo: str):
+    archivo = open(nombre_archivo, "r")
+    destino = open("reverso.txt", "w", encoding = "utf8")
+    destino.truncate()
+
+    for linea in reversed(archivo.readlines()):
+        destino.write(linea)
+    #.reverse() no se lo puede usar para elementos iterables pero si reversed(), notar que reverse() cambia la lista en su lugar y solo deberiamos llamar a la funcion que le aplicamos el reverse, pero el reversed() crea una copia
+
+    archivo.close()
+    destino.close()
+
+#print(textoReverso("archivotest.txt"))
+
+#2.8
+def generarNrosAlAzar(n: int, desde: int, hasta: int) -> list[int]:
+    lista: list[int] = []
+
+    for i in range(desde, hasta + 1):
+        lista.append(i)
+
+    listaRandom: list[int] = random.sample(lista, n)
+
+    return(listaRandom)
+
+#print(generarNrosAlAzar(5,2,6)) #es una lista pues .append() es de lista
+
+#2.9 ¿Armar una pila con los numeros generados al azar del punto anterior?
+def armarPilaDeNrosAlAzar(n: int, desde: int, hasta: int) -> Pila:
+    pila = Pila()
+    lista: list[int] = generarNrosAlAzar(n,desde,hasta)
+    for i in lista:
+        pila.put(i)
+
+    return pila
+
+def armarPilaDeLista2(lista: list[int]) -> Pila:
+    pila = Pila()
+    for n in lista:
+        pila.put(n)
+
+    return pila
+
+#print(armarPilaDeLista2([1,2,2]))
+#print(armarPilaDeNrosAlAzar(3,2,6))
+#¿Para qué? Pues generarNrosAlAzar() nos crea una lista con .append() y con LifoQueue .put() no aseguramos que es una pila de alguna forma y no una lista
+
+#2.10
+def cantidadElementos(p: Pila) -> int:
+    return p.qsize()
+
+#print(cantidadElementos(armarPila(generarNrosAlAzar(3,2,6))))
+#print(cantidadElementos(generarNrosAlAzar(3,2,6))) -> no es valido pues es una lista
+
+#2.11
+def buscarElMaximo(p: Pila) -> int:
+    elMaximo: int = 0
+
+    while not(p.empty()):
+        currentElement = p.get()
+        if currentElement > elMaximo:
+            elMaximo = currentElement
+    
+    return elMaximo
+
+print(buscarElMaximo(armarPilaDeLista2([1,2,5,999])))
+
+#2.12
+#def estaBienBalanceada(s: str) -> bool:
