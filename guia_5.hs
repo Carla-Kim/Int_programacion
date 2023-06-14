c392c5f579943c6f3a86d78103da1258be36001c
Carla Kim <carlakim138@gmail.com>
2023-06-14 18:27:57 -0300

    add: new functions

--- guia_5.hs
+++ guia_5.hs
@@ -26,8 +26,8 @@ reverso (x:xs) = reverso(xs) ++ (x : [])
 --2.1
 pertenece :: (Eq t) => t -> [t] -> Bool
 pertenece _ [] = False
-pertenece x lista | head(lista) == x = True
-                  | otherwise = pertenece x (x:lista)
+pertenece x (y:lista) | y == x = True
+                      | otherwise = pertenece x lista
 
 pertenece1 :: (Eq t) => t -> [t] -> Bool 
 pertenece1 valorABuscar lista | longitud(lista) == 0 = False
@@ -35,8 +35,11 @@ pertenece1 valorABuscar lista | longitud(lista) == 0 = False
                               | otherwise = pertenece valorABuscar (tail lista)
 
 --2.2 
-{- todosIguales :: (Eq t) => [t] -> Bool
-todosIguales 
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [x] = True
+todosIguales (x:y:resto) | x == y = todosIguales (y:resto)
+                         | otherwise = False
 
 --2.3
 todosDiferentes :: (Eq t) => [t] -> Bool
@@ -45,4 +48,40 @@ todosDiferentes lista | not(todosIguales lista) = True
 
 --2.4
 hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos -}
+hayRepetidos [] = False
+hayRepetidos [x] = False
+hayRepetidos (x:y:lista) | x == y = True
+                         | otherwise = hayRepetidos lista
+
+--2.5
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar elem (x:lista) | elem == x = lista
+                      | otherwise = x : quitar elem lista
+
+--2.6
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos elem lista | pertenece elem lista == False = lista
+                       | pertenece elem lista == True = quitarTodos elem (quitar elem lista)
+
+--2.7
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:lista) = x : eliminarRepetidos(quitarTodos x lista)
+
+--2.8
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos lista1 lista2 | longitud(eliminarRepetidos(lista1)) == longitud(eliminarRepetidos(lista2)) = mismosElementosAux lista1 lista2
+                              | otherwise = False
+
+mismosElementosAux :: (Eq t) => [t] -> [t] -> Bool
+mismosElementosAux [] _ = True
+mismosElementosAux (x:lista1) lista2 | pertenece x lista2 = mismosElementosAux lista1 lista2
+                                     | otherwise = False
+
+--3.3
+maximo :: [Integer] -> Integer
+maximo [x] = x
+maximo (x : lista) | x > maximo(lista) = x 
+                   | otherwise = maximo lista
